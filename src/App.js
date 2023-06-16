9b4437b570b9b80333ead91cfabb1b4be51ff73b
AlejandroHernandez1802 <alejandro.herqui@gmail.com>
2023-06-15 20:43:24 -0500

    Implementing data saving using json-server and axios for requests.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 /*Hooks imports*/
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+/*Axios import*/
+import axios from "axios";
 
 /*Styles import*/
 import "./assets/App.css";
@@ -12,24 +15,45 @@ function App() {
 	/*States declaration*/
 	const [books, setBooks] = useState([]);
 
+	/*Mounted functions - declaration*/
+	const getBooks = async () => {
+		const response = await axios.get("http://localhost:3001/books");
+		setBooks(response.data);
+	};
+
+	/*Mounted functions - call*/
+	useEffect(() => {
+		getBooks();
+	}, []);
+
 	/*States handlers methods */
-	const createBook = (title) => {
-		const booksArray = [
-			...books,
-			{ id: Math.round(Math.random() * 999999), title },
-		];
+	const createBook = async (title) => {
+		//Axios usage
+		const response = await axios.post("http://localhost:3001/books", { title });
+
+		//Local usage
+		const booksArray = [...books, response.data];
 		setBooks(booksArray);
 	};
 
-	const deleteBookById = (id) => {
+	const deleteBookById = async (id) => {
+		//Axios usage
+		await axios.delete(`http://localhost:3001/books/${id}`);
+
+		//Local usage
 		const updatedBooks = books.filter((book) => book.id !== id);
 		setBooks(updatedBooks);
 	};
 
-	const onEditBook = (bookToEdit) => {
+	const onEditBook = async (bookToEdit) => {
+		//Axios usage
+		const response = await axios.put(
+			`http://localhost:3001/books/${bookToEdit.id}`,
+			bookToEdit
+		);
 		const updatedBooks = books.map((book) => {
 			if (book.id === bookToEdit.id) {
-				return { ...book, title: bookToEdit.title };
+				return { ...book, title: response.data.title };
 			}
 			return book;
 		});
